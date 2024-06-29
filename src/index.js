66cb9b759a44775167faa197a4e02a1ef92df30c
darellanodev <darellano.aliaga@gmail.com>
2024-06-29 19:00:45 +0200

    basic html form and interaction with js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("hello task eater manager!")
+console.log("Starting app Task Eater Manager")
+
+import { TodoList } from "./TodoList.js"
+import { Item } from "./Item.js"
+
+globalThis.DOM = {}
+const DOM = globalThis.DOM
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("The DOM Content is loaded and ready")
+  DOM.form = document.getElementById("taskform")
+  DOM.newTask = document.getElementById("newtask")
+  DOM.tasks = document.getElementById("tasks")
+
+  const todoList = new TodoList()
+  DOM.form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    console.log(
+      `Submit button clicked, with new task value: ${DOM.newTask.value}`,
+    )
+    // add task
+    const element = document.createElement("p")
+    element.innerHTML = DOM.newTask.value
+    DOM.tasks.appendChild(element)
+  })
+})
