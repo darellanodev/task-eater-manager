7c76ce06c41de43b41722716b389aa944b2f925c
darellanodev <darellano.aliaga@gmail.com>
2024-06-30 12:45:18 +0200

    convert TodoList in a singleton class

--- src/TodoList.ts
+++ src/TodoList.ts
@@ -1,9 +1,21 @@
 import { Task } from "./Task"
 
 export class TodoList {
+  static #instance: TodoList | null = null
   #tasks: Task[]
   constructor() {
+    if (TodoList.#instance !== null) {
+      throw new Error(
+        "TodoList is a singleton class, use getInstance to get the instance",
+      )
+    }
     this.#tasks = []
+  }
+  static getInstance() {
+    if (TodoList.#instance === null) {
+      TodoList.#instance = new TodoList()
+    }
+    return TodoList.#instance
   }
   get tasks() {
     return this.#tasks
